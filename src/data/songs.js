7db3f9198822c1feb1ea9597e8ba23cb8cbc73da
wawrzaszek <szymonmosor@outlook.com>
2026-06-01 15:01:50 +0200

    Poprawka kategorii Hip-Hop: zmiana ogólnego hasła na konkretnych wykonawców (aby uniknąć m.in. Mike'a Posnera)

--- src/data/songs.js
+++ src/data/songs.js
@@ -2,7 +2,7 @@
 export const categories = [
   { id: 'radio', name: { pl: 'Radio Hits', en: 'Radio Hits' }, color: '#ff4b4b', searchTerm: ['pop', 'party songs', '80s', '90s', '00s', '10s'] },
   { id: 'rock', name: { pl: 'Klasyczny Rock', en: 'Classic Rock' }, color: '#fca311', searchTerm: ['classic rock', 'metal'] },
-  { id: 'hiphop', name: { pl: 'Hip-Hop', en: 'Hip-Hop' }, color: '#2a9d8f', searchTerm: 'hip-hop' },
+  { id: 'hiphop', name: { pl: 'Hip-Hop', en: 'Hip-Hop' }, color: '#2a9d8f', searchTerm: ['rap hits', 'oldschool hip-hop', '2pac', 'eminem', 'snoop dogg', 'dr dre', 'notorious b.i.g.', 'kendrick lamar', '50 cent', 'jay-z', 'nas', 'wu-tang clan'] },
   // Dodanie kategorii Polska Muzyka (będzie losować jedno z haseł przy każdym zapytaniu)
   { id: 'plmusic', name: { pl: 'Polska Muzyka', en: 'Polish Music' }, color: '#e53935', searchTerm: ['dżem', 'kombi', 'lady pank', 'perfect', 'budka suflera', 'zespół akcent', 'zespół boys', 'sławomir', 'zenon martyniuk', 'disco polo', 'krzysztof krawczyk', 'maryla rodowicz', 'sanah', 'dawid podsiadło', 'polski pop'] }
 ];
