1d22205037bfc9a4dd98cb88b9e356860137b205
wawrzaszek <szymonmosor@outlook.com>
2026-06-01 14:55:19 +0200

    Poprawka polskich zespołów (Akcent i Boys) w zapytaniach do API

--- src/data/songs.js
+++ src/data/songs.js
@@ -4,7 +4,7 @@ export const categories = [
   { id: 'rock', name: { pl: 'Klasyczny Rock', en: 'Classic Rock' }, color: '#fca311', searchTerm: ['classic rock', 'metal'] },
   { id: 'hiphop', name: { pl: 'Hip-Hop', en: 'Hip-Hop' }, color: '#2a9d8f', searchTerm: 'hip-hop' },
   // Dodanie kategorii Polska Muzyka (będzie losować jedno z haseł przy każdym zapytaniu)
-  { id: 'plmusic', name: { pl: 'Polska Muzyka', en: 'Polish Music' }, color: '#e53935', searchTerm: ['dżem', 'kombi', 'lady pank', 'perfect', 'budka suflera', 'akcent', 'boys', 'sławomir', 'zenon martyniuk', 'disco polo', 'krzysztof krawczyk', 'maryla rodowicz', 'sanah', 'dawid podsiadło', 'polski pop'] }
+  { id: 'plmusic', name: { pl: 'Polska Muzyka', en: 'Polish Music' }, color: '#e53935', searchTerm: ['dżem', 'kombi', 'lady pank', 'perfect', 'budka suflera', 'zespół akcent', 'zespół boys', 'sławomir', 'zenon martyniuk', 'disco polo', 'krzysztof krawczyk', 'maryla rodowicz', 'sanah', 'dawid podsiadło', 'polski pop'] }
 ];
 
 // Funkcja pobierająca losowe utwory z iTunes API (wyklucza te już odgadnięte)
